dd02e554ea207b14fb0b3bd2ce8798fad4ac45f5
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2024-01-12 12:32:29 +0900

    Initial Commit

---
+++ USBNotifier/USBNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  USBNotifierApp.swift
+//  USBNotifier
+//
+//  Created by Venti on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct USBNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
